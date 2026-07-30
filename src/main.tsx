9b3c9a27ea8fc9fee4fd205fb252a05558370f96
ono <ono1996@163.com>
2026-07-30 20:49:38 +0800

    ci(base): 🚀 将打包产物上传到公开仓库

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { router } from '@/router'
 import '@unocss/reset/tailwind.css'
-import 'ono-react-element/index.css'
+import 'ono-react-element/dist/index.css'
+// import "ono-react-element/index.css"
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 import 'virtual:uno.css'
